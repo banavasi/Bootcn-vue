ce5c866e59a82a1d5d04dd3289f5a056cde33a35
banavasi <shashank.v.shandilya@gmail.com>
2026-01-10 14:00:58 -0700

    feat(playground): added the example folder with base button implemented

---
+++ examples/demo-bootcn/src/components/HelloWorld.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { Button } from "@/components/ui/Button";
+defineProps<{
+  msg: string;
+}>();
+</script>
+
+<template>
+  <div class="greetings">
+    <h1 class="green">{{ msg }}</h1>
+    <h3>
+      You’ve successfully created a project with
+      <Button asChild variant="primary"
+        ><a href="https://vite.dev/" target="_blank" rel="noopener">Vite sdsfadsf</a></Button
+      >
+    </h3>
+
+    <Button variant="secondary">Secondary Button</Button>
+  </div>
+</template>
