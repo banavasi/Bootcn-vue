ff424c88705d23b449b012d80a3f3b1397180dad
banavasi <shashank.v.shandilya@gmail.com>
2026-01-12 22:48:41 -0700

    test(forms): add input password toggle functionality tests

--- packages/forms/src/primitives/InputPassword/InputPassword.spec.ts
+++ packages/forms/src/primitives/InputPassword/InputPassword.spec.ts
@@ -16,4 +16,50 @@ describe("InputPassword", () => {
     const input = wrapper.find('input[type="password"]');
     expect(input.exists()).toBe(true);
   });
+
+  it("toggles between password and text type when button clicked", async () => {
+    const wrapper = mount(InputRoot, {
+      props: { id: "test-pwd" },
+      slots: {
+        default: () => h(InputPassword),
+      },
+    });
+
+    const input = wrapper.find("input");
+    const button = wrapper.find("button");
+
+    expect(input.attributes("type")).toBe("password");
+
+    await button.trigger("click");
+    expect(input.attributes("type")).toBe("text");
+
+    await button.trigger("click");
+    expect(input.attributes("type")).toBe("password");
+  });
+
+  it("shows eye icon when password is hidden", () => {
+    const wrapper = mount(InputRoot, {
+      props: { id: "test-pwd" },
+      slots: {
+        default: () => h(InputPassword),
+      },
+    });
+
+    const button = wrapper.find("button");
+    expect(button.attributes("aria-label")).toBe("Show password");
+  });
+
+  it("shows eye-slash icon when password is visible", async () => {
+    const wrapper = mount(InputRoot, {
+      props: { id: "test-pwd" },
+      slots: {
+        default: () => h(InputPassword),
+      },
+    });
+
+    const button = wrapper.find("button");
+    await button.trigger("click");
+
+    expect(button.attributes("aria-label")).toBe("Hide password");
+  });
 });
